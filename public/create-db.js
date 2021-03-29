3409778cbf087380ad7803a14aa9198954627247
Glitch (hello-express) <none>
2021-03-29 13:47:50 +0000

    🌒📡 Checkpoint ./public/create-db.js:30043245/1020

--- public/create-db.js
+++ public/create-db.js
@@ -7,8 +7,12 @@ db.prepare('DROP TABLE IF EXISTS category').run();
 db.prepare('DROP TABLE IF EXISTS state').run();
 db.prepare('DROP TABLE IF EXISTS user').run();
 db.prepare('DROP TABLE IF EXISTS challenge').run();
+db.prepare('DROP TABLE IF EXISTS acceptedchallenges').run();
 
 db.prepare('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, bonusPoints INTEGER)').run();
 db.prepare('CREATE TABLE user (username TEXT PRIMARY KEY, password TEXT, profilePic TEXT, points INTEGER, isAdmin INTEGER)').run();
 db.prepare('CREATE TABLE state (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)').run();
-db.prepare('CREATE TABLE challenge (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, nbUpvotes INTEGER, nbReports INTEGER)').run();
+db.prepare('CREATE TABLE challenge (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, nbUpvotes INTEGER, nbReports INTEGER, category INTEGER, state INTEGER, user TEXT,'
+          + 'FOREIGN KEY (category) REFERENCES category(id), FOREIGN KEY (state) REFERENCES state(id), FOREIGN KEY (user) REFERENCES user(username))').run();
+db.prepare('CREATE TABLE acceptedchallenges (challengeid INTEGER PRIMARY KEY, username TEXT PRIMARY KEY, '
+          + 'PRIMARY KEY(FOREIGN KEY (challengeid) REFERENCES challenge(id), FOREIGN KEY (username) REFERENCES user(username))').run();
