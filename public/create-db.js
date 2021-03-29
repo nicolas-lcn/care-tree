571820adf87a2bc9c4b3ab252e96afb90b6510c7
Glitch (hello-express) <none>
2021-03-29 13:35:45 +0000

    🛫⛑ Checkpoint ./public/create-db.js:30043245/1063

--- public/create-db.js
+++ public/create-db.js
@@ -3,7 +3,12 @@ const Sqlite = require('better-sqlite3');
 let db = new Sqlite('db.sqlite');
 
 
-b.prepare('DROP TABLE IF EXISTS recipe').run();
-  db.prepare('DROP TABLE IF EXISTS ingredient').run();
-  db.prepare('DROP TABLE IF EXISTS stage').run();
-  db.prepare('DROP TABLE IF EXISTS user').run();
+db.prepare('DROP TABLE IF EXISTS category').run();
+db.prepare('DROP TABLE IF EXISTS state').run();
+db.prepare('DROP TABLE IF EXISTS user').run();
+db.prepare('DROP TABLE IF EXISTS challenge').run();
+
+db.prepare('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, bonusPoints INTEGER)').run();
+db.prepare('CREATE TABLE user (username TEXT PRIMARY KEY, password TEXT, profilePic TEXT, points INTEGER, isAdmin INTEGER)').run();
+db.prepare('CREATE TABLE state (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)').run();
+db.prepare('CREATE TABLE challenge (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, nbUpvotes INTEGER, nbReports INTEGER)').run();
