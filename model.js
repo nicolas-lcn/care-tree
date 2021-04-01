3cccd2cfc59715a1652124f4796cad9164a1b790
Glitch (hello-express) <none>
2021-04-01 09:19:15 +0000

    🎸🚶 Checkpoint ./model.js:30048308/3095 ./views/login.html:30048308/46 ./server.js:30048308/2

--- model.js
+++ model.js
@@ -1,28 +1,39 @@
-"use strict"
-const Sqlite = require('better-sqlite3');
+"use strict";
+const Sqlite = require("better-sqlite3");
 
-let db = new Sqlite('db.sqlite');
+let db = new Sqlite("db.sqlite");
 
-exports.getChallenges = (page) => {
+exports.getChallenges = page => {
   const num_per_page = 4;
   page = parseInt(page || 1);
 
-  
-  var num_found = db.prepare('SELECT count(*) FROM challenge').get()['count(*)'];
-  var results = db.prepare('SELECT challenge.id, title, description, nbUpvotes, category.name AS categoryName, state, username '
-                           + 'FROM challenge JOIN category ON challenge.category = category.id '
-                           + 'JOIN state ON challenge.state = state.id '
-                           + 'JOIN user ON challenge.user = user.id '
-                           + 'ORDER BY challenge.id LIMIT ? OFFSET ?').all(num_per_page, (page - 1) * num_per_page);
+  var num_found = db.prepare("SELECT count(*) FROM challenge").get()[
+    "count(*)"
+  ];
+  var results = db
+    .prepare(
+      "SELECT challenge.id, title, description, nbUpvotes, category.name AS categoryName, state, username " +
+        "FROM challenge JOIN category ON challenge.category = category.id " +
+        "JOIN state ON challenge.state = state.id " +
+        "JOIN user ON challenge.user = user.id " +
+        "ORDER BY challenge.id LIMIT ? OFFSET ?"
+    )
+    .all(num_per_page, (page - 1) * num_per_page);
 
   return {
     results: results,
     num_found: num_found,
-    prev_page: (page > 1) ? page - 1 : 1,
-    next_page: (page * num_per_page <= num_found)? page + 1 : page,
+    prev_page: page > 1 ? page - 1 : 1,
+    next_page: page * num_per_page <= num_found ? page + 1 : page,
     page: page,
-    num_pages: parseInt(num_found / num_per_page) + 1,
+    num_pages: parseInt(num_found / num_per_page) + 1
   };
-  
-exports.login = ()
+};
+
+exports.login = (username, password) => {
+  let select = db
+    .prepare("SELECT rowid FROM users WHERE username = ? AND password = ?")
+    .get(username, password);
+  if (select) return select.rowid;
+  return -1;
 };
