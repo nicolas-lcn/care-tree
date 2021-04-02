c136bd7517cdc77aeaa84ff71666ad06d773f691
Glitch (hello-express) <none>
2021-04-02 08:24:31 +0000

    🌴🎓 Checkpoint ./server.js:30043245/18:30048308/4373

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-"use strict"
+"use strict";
 
-var express = require('express');
-var mustache = require('mustache-express');
-const cookieSession = require('cookie-session');
+var express = require("express");
+var mustache = require("mustache-express");
+const cookieSession = require("cookie-session");
 
-var model = require('./model');
+var model = require("./model");
 var app = express();
 
+app.use(
+  cookieSession({
+    name: "session",
+    secret: "bGtrZ8a5gh6e58g75dgd47zVZDsH75FSsa5"
+  })
+);
 
 function update_locals(req, res, next) {
   console.log(req.session.name);
-  if(req.session.name) {
+  if (req.session.name) {
     res.locals.authenticated = true;
     res.locals.name = req.session.name;
   }
@@ -18,79 +24,68 @@ function update_locals(req, res, next) {
 }
 app.use(update_locals);
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//TODO: random secret
-app.use(cookieSession({
-  name :'session',
-  secret:'bGtrZ8a5gh6e58g75dgd47zVZDsH75FSsa5',
-}));
-
-
-app.engine('html', mustache());
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+app.engine("html", mustache());
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
 
 function is_authenticated(req, res, next) {
-  if(req.session.name != null) {
+  if (req.session.name != null) {
     return next();
   }
-  res.status(401).send('Authentication required');
+  res.status(401).send("Authentication required");
 }
 
 /**** Routes pour voir les pages du site ****/
 
-
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/challenges', (req, res) => {
+app.get("/challenges", (req, res) => {
   var found = model.getChallenges(req.query.page);
-  res.render('challenges', found);
+  res.render("challenges", found);
 });
 
 //////////////////////////////////////////////////////////////////////////////////
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get('/login', (req, res) => {
-  res.render('login');
-})
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
-app.get('/signup', (req, res) => {
-  res.render('signup');
-})
-
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   let username = model.login(req.body.username, req.body.password);
   if (username == null) {
-    res.redirect('/login');
+    res.redirect("/login");
   } else {
     req.session.name = req.body.username;
-    console.log(req.session.name)
-    res.redirect('/');
+    console.log(req.session.name);
+    res.redirect("/");
   }
-})
+});
 
-app.post('/signup', (req, res) => {
+app.post("/signup", (req, res) => {
   let id = model.new_user(req.body.username, req.body.password);
   if (id != undefined) {
     req.session.username = req.body.username;
     req.session.user = id;
-    res.redirect('/');
+    res.redirect("/");
   } else {
-    res.redirect('/new_user');
+    res.redirect("/new_user");
   }
-})
+});
 
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   req.session = null;
-  res.redirect('/');
-})
+  res.redirect("/");
+});
 
 /**** Routes pour modifier les données ****/
 
-app.listen(3000, () => console.log('listening on http://localhost:3000'));
-
-
+app.listen(3000, () => console.log("listening on http://localhost:3000"));
