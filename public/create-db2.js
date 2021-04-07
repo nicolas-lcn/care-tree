5b5b0bd0e56885f9cc6a6599ce09335a1f4784da
Glitch (hello-express) <none>
2021-04-07 14:20:00 +0000

    🕰🕹 Checkpoint ./public/create-db2.js:30043245/343 ./views/profile.html:30048308/740 ./server.js:30048308/4

--- public/create-db2.js
+++ public/create-db2.js
@@ -13,12 +13,15 @@ db.prepare('DROP TABLE IF EXISTS category').run();
 db.prepare('DROP TABLE IF EXISTS state').run();
 db.prepare('DROP TABLE IF EXISTS user').run();
 db.prepare('DROP TABLE IF EXISTS acceptedchallenges').run();
+db.prepare('DROP TABLE IF EXISTS terminatedchallenges').run();
 
 db.prepare('CREATE TABLE user (username TEXT PRIMARY KEY, password TEXT, profilePic TEXT, isAdmin INTEGER)').run();
 db.prepare('CREATE TABLE state (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)').run();
 db.prepare('CREATE TABLE challenge (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, nbUpvotes INTEGER, nbReports INTEGER, state INTEGER, author TEXT, expireDate INTEGER, '
           + 'FOREIGN KEY (state) REFERENCES state(id), FOREIGN KEY (author) REFERENCES user(username))').run();
 db.prepare('CREATE TABLE acceptedchallenges (challengeid INTEGER, username TEXT, '
+          + 'PRIMARY KEY(challengeid, username), FOREIGN KEY (challengeid) REFERENCES challenge(id), FOREIGN KEY (username) REFERENCES user(username))').run();
+db.prepare('CREATE TABLE terminatedchallenges (challengeid INTEGER, username TEXT, '
           + 'PRIMARY KEY(challengeid, username), FOREIGN KEY (challengeid) REFERENCES challenge(id), FOREIGN KEY (username) REFERENCES user(username))').run();
 
 let username1 = "SuperMarmotton";
@@ -30,9 +33,9 @@ let password = crypt_password('1234');
 
 db.prepare('INSERT INTO user VALUES (?, ?, \'\', 1)').run('admin', password);
 db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0)').run(username1, password).lastInsertRowid;
-db.prepare('INSERT INTO user VALUES (?, ?, \'\', 00)').run(username2, password).lastInsertRowid;
-db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0, 0)').run(username3, password).lastInsertRowid;
-db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0, 0)').run(username4, password).lastInsertRowid;
+db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0)').run(username2, password).lastInsertRowid;
+db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0)').run(username3, password).lastInsertRowid;
+db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0)').run(username4, password).lastInsertRowid;
 
 let open = db.prepare('INSERT INTO state (name) VALUES (\'OPEN\')').run().lastInsertRowid;
 let reported = db.prepare('INSERT INTO state (name) VALUES (\'REPORTED\')').run().lastInsertRowid;
