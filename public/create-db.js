d5784cebfdf93c123d91be7ead1456f943134174
Glitch (hello-express) <none>
2021-04-10 14:15:59 +0000

    🖖🚞 Checkpoint ./public/create-db.js:30043245/2065

--- public/create-db.js
+++ public/create-db.js
@@ -17,7 +17,7 @@ db.prepare('DROP TABLE IF EXISTS user').run();
 
 db.prepare('CREATE TABLE user (username TEXT PRIMARY KEY, password TEXT, profilePic TEXT, isAdmin INTEGER)').run();
 db.prepare('CREATE TABLE state (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)').run();
-db.prepare('CREATE TABLE challenge (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, nbUpvotes INTEGER, nbReports INTEGER, state INTEGER, author TEXT, expireDate INTEGER, '
+db.prepare('CREATE TABLE challenge (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, state INTEGER, author TEXT, expireDate INTEGER, '
           + 'FOREIGN KEY (state) REFERENCES state(id), FOREIGN KEY (author) REFERENCES user(username))').run();
 db.prepare('CREATE TABLE userchallenge (challengeid INTEGER, username TEXT, isLiked INTEGER, isReported INTEGER, isAccepted INTEGER, isSucceeded INTEGER'
           + 'PRIMARY KEY(challengeid, username), FOREIGN KEY (challengeid) REFERENCES challenge(id), FOREIGN KEY (username) REFERENCES user(username))').run();
@@ -35,32 +35,41 @@ db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0)').run(username2, password);
 db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0)').run(username3, password);
 db.prepare('INSERT INTO user VALUES (?, ?, \'\', 0)').run(username4, password);
 
-let accepted = db.prepare('INSERT INTO state (name) VALUES (\'ACCEPTED\')').run().lastInsertRowid;
-let succeeded = db.prepare('INSERT INTO state (name) VALUES (\'SUCCEEDED\')').run().lastInsertRowid;
+let open = db.prepare('INSERT INTO state (name) VALUES (\'OPEN\')').run().lastInsertRowid;
+let suspended = db.prepare('INSERT INTO state (name) VALUES (\'SUSPENDED\')').run().lastInsertRowid;
+let closed = db.prepare('INSERT INTO state (name) VALUES (\'CLOSED\')').run().lastInsertRowid;
 
 
-let expireDate = Date.now() + 24 * 60 * 60 * 1000 * 7;
+
+let expireDate = Date.now() + 24 * 60 * 60 * 1000 * 7; // 7 days
 
 // INSERT CHALLENGE
 
-db.prepare("INSERT INTO challenge (title, description, nbUpvotes, nbReports, state, author, expireDate) VALUES "
-           + "(?, ?, ?, ?, ?, ?, ?)").run('Se débarrasser de ses vieux vêtements', 'Donnez-les ou revendez-les !', 5, 0, open, username1, expireDate);
-db.prepare("INSERT INTO challenge (title, description, nbUpvotes, nbReports, state, author, expireDate) VALUES "
-           + "(?, ?, ?, ?, ?, ?, ?)").run('Donner son sang', 'Donner son sang permet de sauver 3 vies !', 10, 0, open, username2, expireDate);
-db.prepare("INSERT INTO challenge (title, description, nbUpvotes, nbReports, state, author, expireDate) VALUES "
-           + "(?, ?, ?, ?, ?, ?, ?)").run('Complimenter un inconnu', '', 3, 0, open, username3, expireDate);
-db.prepare("INSERT INTO challenge (title, description, nbUpvotes, nbReports, state, author, expireDate) VALUES "
-           + "(?, ?, ?, ?, ?, ?, ?)").run('No meat !', 'Ne pas manger de viande pendant toute une semaine.', 15, 1, open, username4, expireDate);
-db.prepare("INSERT INTO challenge (title, description, nbUpvotes, nbReports, state, author, expireDate) "
-           + "VALUES (?, ?, ?, ?, ?, ?, ?)").run("Donner 20€ à l'association de votre choix", 'Tout est dit dans le titre :)', 15, 1, open, username1, expireDate);
+let chall1 = db.prepare("INSERT INTO challenge (title, description, state, author, expireDate) VALUES "
+           + "(?, ?, ?, ?, ?, ?, ?)").run('Se débarrasser de ses vieux vêtements', 'Donnez-les ou revendez-les !', open, username1, expireDate).lastInsertRowid;
+let chall2 = db.prepare("INSERT INTO challenge (title, description, state, author, expireDate) VALUES "
+           + "(?, ?, ?, ?, ?, ?, ?)").run('Donner son sang', 'Donner son sang permet de sauver 3 vies !', open, username2, expireDate).lastInsertRowid;
+let chall3 = db.prepare("INSERT INTO challenge (title, description, state, author, expireDate) VALUES "
+           + "(?, ?, ?, ?, ?, ?, ?)").run('Complimenter un inconnu', '', open, username3, expireDate).lastInsertRowid;
+let chall4 = db.prepare("INSERT INTO challenge (title, description, state, author, expireDate) VALUES "
+           + "(?, ?, ?, ?, ?, ?, ?)").run('No meat !', 'Ne pas manger de viande pendant toute une semaine.', open, username4, expireDate).lastInsertRowid;
+let chall5 = db.prepare("INSERT INTO challenge (title, description, state, author, expireDate) "
+           + "VALUES (?, ?, ?, ?, ?, ?, ?)").run("Donner 20€ à l'association de votre choix", 'Tout est dit dans le titre :)', open, username1, expireDate).lastInsertRowid;
 
-db.prepare("INSERT INTO challenge (title, description, nbUpvotes, nbReports, state, author, expireDate) "
-           + "VALUES (?, ?, ?, ?, ?, ?, ?)").run("This challenge should not appear !", 'If you see this, there is a problem somewhere...', 0, 0, open, username1, Date.now() - 1000);
-db.prepare("INSERT INTO challenge (title, description, nbUpvotes, nbReports, state, author, expireDate) "
-           + "VALUES (?, ?, ?, ?, ?, ?, ?)").run("This challenge should not appear !", 'If you see this, there is a problem somewhere...', 0, 0, closed, username1, expireDate);
+let chall6 = db.prepare("INSERT INTO challenge (title, description, state, author, expireDate) "
+           + "VALUES (?, ?, ?, ?, ?, ?, ?)").run("This challenge should not appear !", 'If you see this, there is a problem somewhere...', 0, 0, open, username1, Date.now() - 1000).lastInsertRowid;
+let chall7 = db.prepare("INSERT INTO challenge (title, description, state, author, expireDate) "
+           + "VALUES (?, ?, ?, ?, ?, ?, ?)").run("This challenge should not appear !", 'If you see this, there is a problem somewhere...', 0, 0, closed, username1, expireDate).lastInsertRowid;
 
 
 // INSERT USERCHALLENGE
-db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?)").run(1, username1, 0, 1, )
-db.prepare("INSERT INTO acceptedchallenges VALUES (?, ?)").run(3, username2)
-db.prepare("INSERT INTO acceptedchallenges VALUES (?, ?)").run(4, username2)
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
+db.prepare("INSERT INTO userchallenge VALUES (?, ?, ?, ?, ?, ?)").run(chall1, username1, 1, 0, 0, 0);
