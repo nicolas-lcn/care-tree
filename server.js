c0608469731de42da573affd927f42548c0b8fe3
Glitch (hello-express) <none>
2021-04-14 12:51:22 +0000

    🏩🔮 Checkpoint ./server.js:30043245/34 ./views/challenges.html:30043245/54

--- server.js
+++ server.js
@@ -103,8 +103,8 @@ app.get("/acceptChallenge/:id", is_authenticated, (req, res) => {
   res.render("acceptedChallenges", acceptedChallenges);
 });
 
-app.get("/acceptChallenge/:id", is_authenticated, (req, res) => {
-  let success = model.acceptChallenge(req.session.name, req.params.id);
+app.get("/endChallenge/:id", is_authenticated, (req, res) => {
+  let success = model.endChallenge(req.session.name, req.params.id);
   let acceptedChallenges = model.getAcceptedChallenges(req.query.page, req.session.name);
   if (success) acceptedChallenges.success = {msg : "Défi accepté !"};
   res.render("acceptedChallenges", acceptedChallenges);
