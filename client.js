875907521d98eefc7bfa6720a011dd0803d1db8a
Glitch (hello-express) <none>
2021-04-15 12:50:57 +0000

    💪🚏 Checkpoint ./views/footer.html:30048308/30 ./server.js:30048308/12 ./client.js:30048308/596

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
-function clicked(element, isliked, nbUpvotes){
-     element.firstElementChild.className = (isliked)? "far fa-thumbs-up fa-lg" : "fas fa-thumbs-up fa-lg";
-     element.lastElementChild.innerText= (isliked)? nbUpvotes-1 : nbUpvotes+1;
-     //todo : add the feature to re-like an unliked challenge
+function clicked(element, isliked, nbUpvotes) {
+  element.firstElementChild.className = isliked
+    ? "far fa-thumbs-up fa-lg"
+    : "fas fa-thumbs-up fa-lg";
+  element.lastElementChild.innerText = isliked ? nbUpvotes - 1 : nbUpvotes + 1;
+  //todo : add the feature to re-like an unliked challenge
 }
