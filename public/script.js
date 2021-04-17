a7628b4d03174d70834bcd51b80363d77f17fcc5
Glitch (hello-express) <none>
2021-04-17 09:09:12 +0000

    📼🍂 Checkpoint ./public/script.js:30043245/305

--- public/script.js
+++ public/script.js
@@ -5,3 +5,19 @@ function clicked(element, isliked, nbUpvotes) {
   element.lastElementChild.innerText = isliked ? nbUpvotes - 1 : nbUpvotes + 1;
   //todo : add the feature to re-like an unliked challenge
 }
+
+function readfile() {
+var data = {};
+data.path = '/home/test/pgadmin.txt';
+data.ext = '.txt';
+console.log(data);
+  $.ajax({
+    url: '/read_file',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    success: function(data) {
+      console.log(data);
+    }
+  });
+}
